b9edad9cb22bf86959457fe4c8c02e33d41a690a
Jeff Eng <jeff@jeffeng.me>
2016-08-06 15:39:20 -0700

    added image to the view and also cycle through animation values

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    
+    @IBOutlet weak var tap: UIButton!
+    @IBAction func tapped(sender: AnyObject) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
     override func didReceiveMemoryWarning() {
