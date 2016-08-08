df86fd7308ea987ffedb964a635b5d46ba097c5b
Jeff Eng <jeff@jeffeng.me>
2016-08-07 22:30:39 -0700

    added all animation cases

--- Project15/Project15/ViewController.swift
+++ Project15/Project15/ViewController.swift
@@ -15,6 +15,36 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tap: UIButton!
     @IBAction func tapped(sender: AnyObject) {
+        tap.hidden = true
+        
+        UIView.animateWithDuration(1, delay: 0, options: [], animations: { [unowned self] in
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransformMakeScale(2, 2)
+            case 1:
+                self.imageView.transform = CGAffineTransformIdentity
+            case 2:
+                self.imageView.transform = CGAffineTransformMakeTranslation(-256, -256)
+            case 3:
+                self.imageView.transform = CGAffineTransformIdentity
+            case 4:
+                self.imageView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+            case 5:
+                self.imageView.transform = CGAffineTransformIdentity
+            case 6:
+                self.imageView.alpha = 0.1
+                self.imageView.backgroundColor = UIColor.greenColor()
+            case 7:
+                self.imageView.alpha = 1.0
+                self.imageView.backgroundColor = UIColor.clearColor()
+            default:
+                break
+            
+            }
+        }) { [unowned self] (finished: Bool) in
+            self.tap.hidden = false
+        }
+        
         currentAnimation += 1
         
         if currentAnimation > 7 {
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CoreAnimationPractice
 This repo follows Project 15: Core Animation at hackingwithswift.com.
+
+## Concepts Learned/Practiced
+* Switch statements
